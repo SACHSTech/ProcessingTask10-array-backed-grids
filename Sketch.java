ad8b7bd630e6dce0b9c2f7eb8a52659ce6492253
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2022-05-12 17:30:04 -0400

    Update Sketch.java

--- Sketch.java
+++ Sketch.java
@@ -1,32 +1,13 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-  // Set how many rows and columns we will have
-  int ROW_COUNT = 10;
-  int COLUMN_COUNT = 10;
-  
-  // This sets the WIDTH and HEIGHT of each grid location
-  int CELL_WIDTH = 20;
-  int CELL_HEIGHT = 20;
-  
-  // This sets the margin between each cell and on the edges of the screen.
-  int MARGIN = 5;
-  
-  // Do the math to figure out our screen dimensions
-  int SCREEN_WIDTH = (CELL_WIDTH + MARGIN) * COLUMN_COUNT + MARGIN;
-  int SCREEN_HEIGHT = (CELL_HEIGHT + MARGIN) * ROW_COUNT + MARGIN;
-
-  // declare a 2D array
-  int[][] intGrid;
-  
-  
-  	
+	
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
-	// put your size call here
-    size(SCREEN_WIDTH, SCREEN_HEIGHT);
+    // put your size call here
+    size(500, 600);
   }
 
   /** 
@@ -34,8 +15,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(0, 0, 0);
-    intGrid = new int[10][10];
+    background(255,255,255);
   }
 
   /**
@@ -43,30 +23,9 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-    strokeWeight(0);
-
-    //intGrid[1][5] = 1;
-
-    for(int row = 0; row < ROW_COUNT; row++){
-      for (int column = 0; column< COLUMN_COUNT; column++){
-        fill(255);
-        if (intGrid[row][column] == 1){
-          fill(0,255,0);
-        }
-        rect((MARGIN + CELL_WIDTH)*column + MARGIN ,(MARGIN + CELL_HEIGHT)*row + MARGIN , CELL_WIDTH, CELL_HEIGHT);
-      }
-    }
-    
-
   }
 
   public void mousePressed(){
-    int intGridRow, intGridColumn;
-    intGridColumn = mouseX/(CELL_WIDTH + MARGIN);
-    intGridRow = mouseY/(CELL_HEIGHT + MARGIN);
-    System.out.println("mouse coordinates: (" + mouseX + ", "+ mouseY + "); grid coordinates: (row:" + intGridRow + ", column: " + intGridColumn + ")");
-    intGrid[intGridRow][intGridColumn] = 1;
-
+    
   }
-
 }
